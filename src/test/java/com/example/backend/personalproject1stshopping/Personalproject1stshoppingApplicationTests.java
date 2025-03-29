8a1140fcd13255de1411773ef8ace04252164110
mlnstone <dnswjs1776@naver.com>
2025-03-29 23:18:53 +0900

    🎉 초기 프로젝트 업로드

---
+++ src/test/java/com/example/backend/personalproject1stshopping/Personalproject1stshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend.personalproject1stshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Personalproject1stshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
