4406ad23f20d3e555af796b8de5b1f2eb625cf1e
mlnstone <dnswjs1776@naver.com>
2025-07-30 22:22:28 +0900

    [7/30] git repository명 변경 테스트

--- src/main/java/com/example/backend/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/example/backend/review/repository/ReviewRepositoryImpl.java
@@ -16,6 +16,7 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+
     @Override
     public Page<Review> findAllByMenu_MenuId(Pageable pageable, Long menuId) {
         QReview review = QReview.review;
